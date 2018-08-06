98fa1082042f4312e666009943d933c7c7b5c704
WAFEL\Dom <nwaszak@wp.pl>
2018-08-06 21:14:32 +0200

    bugfix of method click()

--- my_store.py
+++ my_store.py
@@ -13,6 +13,6 @@ def test_my_store(driver):
     driver.get('http://localhost/litecart/admin/login.php?redirect_url=%2Flitecart%2Fadmin%2F')
     driver.find_element_by_name('username').send_keys('admin')
     driver.find_element_by_name('password').send_keys('admin')
-    driver.find_element_by_name('login').click()
+    driver.find_element_by_name('login').submit()
     WebDriverWait(driver, 10).until(EC.title_is('My Store'))
 

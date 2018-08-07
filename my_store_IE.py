dba505af0407554d3e733fe787928cdadc11017b
WAFEL\Dom <nwaszak@wp.pl>
2018-08-07 20:14:11 +0200

    version for another browser

--- my_store_IE.py
+++ my_store_IE.py
@@ -2,7 +2,14 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
+#create capabilities
+capabilities = DesiredCapabilities.INTERNETEXPLORER
+
+#delete platform and version keys
+capabilities.pop("platform", None)
+capabilities.pop("version", None)
 
 @pytest.fixture
 def driver(request):
@@ -16,4 +23,3 @@ def test_my_store(driver):
     driver.find_element_by_name('password').send_keys('admin')
     driver.find_element_by_name('login').submit()
     WebDriverWait(driver, 10).until(EC.title_is('My Store'))
-

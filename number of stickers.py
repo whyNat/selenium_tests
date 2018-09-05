1a89c1f256daf3e79d7247473b17e38b28e9acc3
WAFEL\Dom <nwaszak@wp.pl>
2018-09-05 18:41:15 +0200

    path fixed

--- number of stickers.py
+++ number of stickers.py
@@ -14,16 +14,16 @@ def test_sticker_products(driver):
     driver.get('http://localhost/litecart/en/')
     driver.find_element_by_xpath('//*[@id="box-category-tree"]/ul/li/a').click()
 
-    list_of_products = driver.find_elements_by_class_name('image img-responsive')
+    list_of_products = driver.find_elements_by_css_selector('.product')
     count_products = len(list_of_products)
-    number = driver.find_elements_by_xpath('//*[@id="box-category"]/img[contains(@class, "sticker")')
+    number = driver.find_elements_by_css_selector('.sticker')
     count_stickers = len(number)
 
     for i in list_of_products:
         if count_products == count_stickers:
-            print('There\'s only than 1 sticker on product')
+            print('There\'s only 1 sticker on product')
         else:
-            print('There\'s more than 1 sticker on product')
+            print('There\'s more 1 sticker on product')
 
 
 
